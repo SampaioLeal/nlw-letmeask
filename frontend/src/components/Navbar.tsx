e88148d0635a73bdb9e2dc4c999d79498d5928c1
Sampaio Leal <samp14hd@gmail.com>
2021-06-21 09:27:21 -0300

    feat: navbar adicionada

---
+++ frontend/src/components/Navbar.tsx
@@ -0,0 +1,49 @@
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  Container,
+  makeStyles,
+} from "@material-ui/core";
+import Logo from "./Logo";
+import RoomCodeButton from "./RoomCodeButton";
+import Spacer from "./Spacer";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    borderBottom: "1px solid",
+    borderBottomColor: "#E2E2E2",
+  },
+  closeButton: {
+    marginLeft: theme.spacing(2),
+  },
+}));
+
+export default function NavBar() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static" color="transparent">
+        <Container>
+          <Toolbar>
+            <Logo height={45} />
+
+            <Spacer />
+
+            <RoomCodeButton />
+
+            <Button
+              className={classes.closeButton}
+              variant="outlined"
+              color="primary"
+              size="small"
+            >
+              Encerrar sala
+            </Button>
+          </Toolbar>
+        </Container>
+      </AppBar>
+    </div>
+  );
+}
