afc8b29becd93daef4d23427d712077e010bdeca
Sampaio Leal <samp14hd@gmail.com>
2021-06-21 01:48:05 -0300

    updated files to monorepo

--- src/components/GoogleIcon.tsx
+++
@@ -1,3 +0,0 @@
-export default function GoogleIcon() {
-  return <img src="/images/googleIcon.png" alt="Ícone do Google" />;
-}
--- src/components/RoomInput.tsx
+++
@@ -1,5 +0,0 @@
-import { TextField, TextFieldProps } from "@material-ui/core";
-
-export default function RoomInput(props: TextFieldProps) {
-  return <TextField variant="outlined" fullWidth {...props} />;
-}
