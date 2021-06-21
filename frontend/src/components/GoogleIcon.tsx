5db7dbfd38fe2d2b3fa9b49a4d6e9386d12ea179
Sampaio Leal <samp14hd@gmail.com>
2021-06-21 01:23:55 -0300

    Inital commit with login page

---
+++ frontend/src/components/GoogleIcon.tsx
@@ -0,0 +1,3 @@
+export default function GoogleIcon() {
+  return <img src="/images/googleIcon.png" alt="Ícone do Google" />;
+}
---
+++ frontend/src/styles/themes/light.ts
@@ -0,0 +1,59 @@
+import { createMuiTheme } from "@material-ui/core";
+import { Shadows } from "@material-ui/core/styles/shadows";
+
+const lightTheme = createMuiTheme({
+  shadows: Array(25).fill("none") as Shadows,
+  palette: {
+    background: {
+      default: "#f8f8f8",
+    },
+    primary: {
+      main: "rgba(131, 90, 253, 1)",
+    },
+    secondary: {
+      main: "#E73F5D",
+    },
+  },
+  typography: {
+    h1: {
+      fontFamily: "Poppins",
+      fontWeight: "bold",
+      fontSize: 36,
+    },
+    h3: {
+      fontSize: 24,
+    },
+  },
+  shape: {
+    borderRadius: 8,
+  },
+  overrides: {
+    MuiButton: {
+      root: {
+        height: 48,
+        fontSize: 16,
+        "& svg": {
+          margin: "0 10px",
+        },
+        "& img": {
+          margin: "0 10px",
+        },
+      },
+      contained: {
+        paddingRight: 32,
+        paddingLeft: 32,
+        color: "rgba(115, 115, 128, 1)",
+      },
+      label: {
+        textTransform: "none",
+        fontWeight: "bold",
+      },
+      sizeSmall: {
+        height: 40,
+        fontSize: 14,
+      },
+    },
+  },
+});
+
+export default lightTheme;
